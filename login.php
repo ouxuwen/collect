383b893994ded67c8b56d5c16167d8bd88a58e3f
ouxuwen <mo22398291@163.com>
2018-10-10 00:18:06 +0800

    update

--- login.php
+++ login.php
@@ -2,47 +2,74 @@
 
 require_once('./base.php');
 
+session_start();
+
 @$username = $_REQUEST['username'] or die('TYPEID REQUIRED');
 @$mobile = $_REQUEST['mobile'] or die('TYPEID REQUIRED');
+@$code = $_REQUEST['code'];
 
+if ($code && isset( $_SESSION['code'])) {
+    if ($_SESSION['code'] != $code) {
+        echo json_encode(
+            [
+                "status" => 0,
+                "msg" => '验证码错误',
+                "data" => '',
+                "code"=>$_SESSION['code'],
+                "time"=>$_SESSION[$mobile]
+            ]
+        );
+        return;
+    }else{
+        $sql = "update user set mobile=$mobile WHERE username='$username' ";
+        $result = mysqli_query($conn, $sql);
+    };
+}else{
+    unset($_SESSION['code']);
+    unset($_SESSION['mobile']);
+}
 
 $sql = "SELECT * FROM user WHERE username='$username' ";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 
 //DQL: false或结果集描述对象
-if($result===false){
-	echo "SELECT ERR: $sql";
-}else {
+if ($result === false) {
+    echo "SELECT ERR: $sql";
+} else {
 	//SQL执行成功，读取所有记录行
-	$list = mysqli_fetch_all($result, MYSQLI_ASSOC);	
-	if($list){
+    $list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    if ($list) {
         $sql = "SELECT * FROM user WHERE username='$username' and mobile = $mobile ";
-        $result = mysqli_query($conn,$sql);
-        if($result===false){
+        $result = mysqli_query($conn, $sql);
+        if ($result === false) {
             echo "SELECT ERR: $sql";
-        }else {
+        } else {
             //SQL执行成功，读取所有记录行
-            $list = mysqli_fetch_all($result, MYSQLI_ASSOC);	
-            if($list){
+            $list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            $date = date("Y-m-d H:i:s"); 
+            $sql = "update user set login_time='$date' WHERE mobile='$mobile' and username='$username' ";
+            $result = mysqli_query($conn, $sql);
+
+            if ($list) {
                 echo json_encode(
                     [
-                        "status"=>1,
-                        "msg"=>'',
-                        "data"=> $list[0]
+                        "status" => 1,
+                        "msg" => '',
+                        "data" => $list[0],
                     ]
-                   );
-            }else{
+                );
+            } else {
                 echo json_encode([
-                    "status"=>0,
-                    "msg"=>'检测到你的手机号码更新了，需要填写验证码哦~'
+                    "status" => 2,
+                    "msg" => '检测到你的手机号码更新了，需要填写验证码哦~'
                 ]);
             }
         }
-	}else{
-		echo json_encode([
-            "status"=>0,
-            "msg"=>'你好像不是我们班的人哦~'
+    } else {
+        echo json_encode([
+            "status" => 0,
+            "msg" => '你好像不是我们班的人哦~'
         ]);
-	}
-	
+    }
+
 }
--- update.php
+++ update.php
@@ -2,27 +2,54 @@
 
 require_once('base.php');
 
-@$mobile = $_REQUEST['mobile'] or die('TYPEID REQUIRED');
-@$password = $_REQUEST['username'] or die('TYPEID REQUIRED');
-@$email = $_REQUEST['email'] or die('TYPEID REQUIRED');
-@$job = $_REQUEST['job'] or die('TYPEID REQUIRED');
-@$major = $_REQUEST['major'] or die('TYPEID REQUIRED');
-@$address = $_REQUEST['address'] or die('TYPEID REQUIRED');
-
-
-$sql = "SELECT * FROM user WHERE mobile='$mobile' and password='$password' ";
-$result = mysqli_query($conn,$sql);
+@$mobile = $_REQUEST['mobile'];
+@$username = $_REQUEST['username'];
+@$email = $_REQUEST['email'];
+@$job = $_REQUEST['job'];
+@$major = $_REQUEST['major'];
+@$address = $_REQUEST['address'];
+@$sex = $_REQUEST['sex'];
+@$notice = $_REQUEST['notice'];
+@$city = $_REQUEST['city'];
+$date = date("Y-m-d H:i:s"); 
+$sql = "update user set email='$email',job='$job',major='$major',address='$address',notice='$notice' ,sex='$sex',city='$city',update_time='$date' WHERE mobile='$mobile' and username='$username' ";
+$result = mysqli_query($conn, $sql);
 
 //DQL: false或结果集描述对象
-if($result===false){
+if ($result === false) {
 	echo "SELECT ERR: $sql";
-}else {
+} else {
 	//SQL执行成功，读取所有记录行
-	$list = mysqli_fetch_all($result, MYSQLI_ASSOC);	
-	if($list){
-		echo json_encode($list[0]);
-	}else{
-		echo json_encode('error');
-	}
+
+	if ($result == 1) {
+       
+		$sql = "SELECT * FROM user WHERE username='$username' and mobile = $mobile ";
+		$result = mysqli_query($conn, $sql);
+		
+        if ($result === false) {
+            echo "SELECT ERR: $sql";
+        } else {
+            //SQL执行成功，读取所有记录行
+			$list = mysqli_fetch_all($result, MYSQLI_ASSOC);
+			echo json_encode(
+				[
+					"status" => 1,
+					"msg" => '更新成功，非常感谢您的支持~',
+					"data" => $list[0],
 	
+				]
+			);
+		}
+		
+	} else {
+		echo json_encode(
+			[
+				"status" => 0,
+				"msg" => '更新失败',
+				"data" => $result,
+
+			]
+		);
+	}
+
 }
